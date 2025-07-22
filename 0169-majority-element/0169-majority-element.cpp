9cf58293427b41c011d58e809b6d597a3dbab1d9
KaushikNamrata <kaushiknamrata016@gmail.com>
2025-07-22 05:42:30 +0530

    Time: 0 ms (100%), Space: 28.1 MB (61.54%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //moore's voting algo
+
+        int freq=0, ans=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(freq==0) {
+                ans=nums[i];
+            }
+
+            if(nums[i]==ans) {
+                freq++;
+            }
+
+            else {
+                freq--;
+            }
+        }
+          return ans;
+    }
+  
+};
