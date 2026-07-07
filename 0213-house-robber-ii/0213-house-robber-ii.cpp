12d209150a7e3cd77924c44e8cf625b567b0aef8
KaushikNamrata <kaushiknamrata016@gmail.com>
2026-07-07 17:23:08 +0530

    Time: 3 ms (6.99%), Space: 10.7 MB (51.7%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int n) {
-        if(i > n)
-            return 0;
-        
-        if(t[i] != -1)
-            return t[i];
-        
-        int take = nums[i] + solve(nums, i+2, n); //steals ith house and moves to i+2 (because we can't steal adjacent)
-        int skip = solve(nums, i+1, n); //skips this house, now we can move to adjacent next house
-        
-        return t[i]=max(take, skip);
-    }
+    int n;
     
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        
+        n = nums.size();
         if(n == 1)
             return nums[0];
         
-        if(n == 2)
-            return max(nums[0], nums[1]);
+        vector<int> t(n+1, 0);
+        //t[i] = Max money gained from i houses
         
-        memset(t, -1, sizeof(t));
+        int result1 = 0;
+        int result2 = 0;
         
-        //case-1 - Take first house 0th index wala house
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        t[0] = 0;
+        for(int i = 1; i<=n-1; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result1 = t[n-1];
         
-        int take_0th_index_house = solve(nums, 0, n-2);
+        t.clear();
+        //Case-2 (Take from 2nd House - Hence take the last house)
+        t[0] = 0;
+        t[1] = 0;
+        for(int i = 2; i<=n; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result2 = t[n];
         
-        memset(t, -1, sizeof(t));
-        
-        //case-2  - Take second house 1st index wala house
-        int take_1st_index_house = solve(nums, 1, n-1);
-        
-        
-        return max(take_0th_index_house, take_1st_index_house);
-        
+        return max(result1, result2);
         
     }
 };
